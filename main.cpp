5a3343c85ba14979cc54218352676e1c1e68c875
younes369 <youns.zied123@gmail.com>
2021-06-15 19:14:35 +0200

    empty

--- main.cpp
+++ main.cpp
@@ -16,20 +16,20 @@ int main()
 {
     int mode;
     cout << "choce a mode:\n";
-    cout << "singel player mode(1)\n";
-    cout << "two player mode(2)\n";
+    cout << "single player mode(1)\n";
+    cout << "two players mode(2)\n";
     cin >> mode;
-    if (mode == 1)
+    if (mode == 2)
     {
         gamePlay();
     }
-    else if (mode == 2)
+    else if (mode == 1)
     {
         AI();
     }
     else
     {
-        cout << "invalid optian";
+        cout << "Invalid optian";
     }
     return 0;
 }
@@ -239,12 +239,12 @@ void board()
     cout << "     |     |     " << endl;
     cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
 
-    cout << "_____|_____|_____" << endl;
+    cout << "__|_|__" << endl;
     cout << "     |     |     " << endl;
 
     cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
 
-    cout << "_____|_____|_____" << endl;
+    cout << "__|_|__" << endl;
     cout << "     |     |     " << endl;
 
     cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
